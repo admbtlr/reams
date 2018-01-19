d73c5c9a7937cae0081b19452505c5a97d95e10e
adamvert <ab@adam-butler.com>
2018-01-19 09:21:56 +0100

    feat: mark divs that directly contain paragraphs as 'content-holder'

--- webview/feed-item.js
+++ webview/feed-item.js
@@ -28,6 +28,29 @@ function markShortBlockquotes() {
   })
 }
 
+function markContentHoldingDivs() {
+  const divs = document.querySelectorAll('div')
+  let isContentHolder
+  let n = 0
+  Array.prototype.forEach.call(divs, function (el) {
+    isContentHolder = false
+    if (el.children) {
+      for (var i = 0; i < el.children.length; i++) {
+        if (el.children[i].tagName === 'P') {
+          isContentHolder = true
+          break
+        }
+      }
+    }
+    if (isContentHolder) {
+      el.classList.add('content-holder')
+      if (!!(n++ % 2)) {
+        el.classList.add('alternating')
+      }
+    }
+  })
+}
+
 var toggleCollapsed = (e) => {
   var expander = document.querySelectorAll('.js-feed-title')[0]
   if (expander.classList.contains('collapsed')) {
@@ -41,6 +64,7 @@ window.onload = function () {
   markShortBlockquotes()
   markShortParagraphs()
   markImages()
+  markContentHoldingDivs()
 
   var expander = document.querySelectorAll('.js-feed-expand')
   document.querySelectorAll('.js-feed-expand')[0].ontouchend = toggleCollapsed
