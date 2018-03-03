b538801db98112ece2593fddf04db08e925e66b8
adamvert <ab@adam-butler.com>
2018-03-03 14:26:55 +0100

    feat: first effort at fetching items for rizzle backend

--- app/redux/backends/rizzle.js
+++ app/redux/backends/rizzle.js
@@ -1,3 +1,9 @@
-export const fetchUnreadItems = () => {}
+export const fetchUnreadItems = (feeds) => {
+  const promises = feeds.map(feed => {
+    const url = `https://api.rizzle.net?url=${feed.url}`
+    return fetch(url)
+  })
+  return Promise.all(promises)
+}
 
 export const markItemRead = (item) => {}
