4ee9a243802f760c9b817ea16d49c29801207a09
adamvert <ab@adam-butler.com>
2018-04-29 12:51:11 +0200

    feat: add new images to buttons

--- app/components/ViewButtons.js
+++ app/components/ViewButtons.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {
   Animated,
+  Image,
   Text,
   TouchableOpacity,
   View
@@ -69,10 +70,13 @@ class ViewButtons extends React.Component {
           }}
           onPress={this.props.increaseFontSize}
         >
-          <Text style={{
-            ...this.getStyles().buttonText,
-            color: saveStrokeColour
-          }}>A-Z</Text>
+          <Image
+            source={require('../img/increase-font-size.png')}
+            style={{
+              position: 'relative',
+              width: 56,
+              height: 56
+            }}/>
         </RizzleButton>
         <RizzleButton
           style={{
@@ -83,11 +87,13 @@ class ViewButtons extends React.Component {
           }}
           onPress={this.props.decreaseFontSize}
           >
-          <Text style={{
-            ...this.getStyles().buttonText,
-            ...this.getStyles().smallText,
-            color: saveStrokeColour
-          }}>A-Z</Text>
+          <Image
+            source={require('../img/decrease-font-size.png')}
+            style={{
+              position: 'relative',
+              width: 56,
+              height: 56
+            }}/>
         </RizzleButton>
         <RizzleButton
           style={{
@@ -98,25 +104,14 @@ class ViewButtons extends React.Component {
           }}
           onPress={this.props.toggleDarkBackground}
           >
-          <Svg
-            height='56'
-            width='56'
+          <Image
+            source={require('../img/night-mode.png')}
             style={{
-              transform: [{
-                translateX: 8
-              },
-              {
-                translateY: 14
-              }]
-            }}>
-            <Path
-              d='M0.5463856,14.8257151 L9.28657345,10.9473761 C11.4036417,7.04700903 14.2125552,4.23809546 17.7133142,2.52063542 C21.2140731,0.803175389 24.952614,0.171289301 28.928937,0.624977161 C29.2413278,4.87808002 28.523773,8.53095226 26.7762726,11.5835939 C25.0287722,14.6362355 22.2198586,17.4451491 18.3495319,20.0103346 L14.0891071,28.3684366 L12.0212829,22.8502948 L9.8182432,24.0975727 L4.73976736,19.0190969 L5.83633186,16.6653438 L0.5463856,14.8257151 Z M6.44060191,22.3026021 L6.65367018,24.2196375 L1.6052423,27.1379617 L4.54274845,22.1087158 L6.44060191,22.3026021 Z M19.009005,9.92845381 C20.0421593,10.9616081 21.7172334,10.9616081 22.7503877,9.92845381 C23.783542,8.8952995 23.783542,7.22022541 22.7503877,6.1870711 C21.7172334,5.1539168 20.0421593,5.1539168 19.009005,6.1870711 C17.9758507,7.22022541 17.9758507,8.8952995 19.009005,9.92845381 Z'
-              fill='none'
-              stroke={saveStrokeColour}
-              strokeLinecap='round'
-              strokeWidth='1'
-            />
-          </Svg>
+              position: 'relative',
+              width: 56,
+              height: 56,
+              left: -4
+            }}/>
         </RizzleButton>
       </Animated.View>
     )
