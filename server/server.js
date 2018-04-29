c4af261355e8ba1882456baac96ee83660208cd0
adamvert <ab@adam-butler.com>
2018-04-29 12:39:33 +0200

    feat(server): handle redirects

--- server/server.js
+++ server/server.js
@@ -37,26 +37,45 @@ app.get('*', (req, res) => {
 
 function fetch (feed, done) {
   let items = []
-  // Define our streams
-  var req = request(feed, {timeout: 10000, pool: false})
-  req.setMaxListeners(50)
-  // Some feeds do not respond without user-agent and accept headers.
-  req.setHeader('user-agent', 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36')
-  req.setHeader('accept', 'text/html,application/xhtml+xml')
 
   var feedparser = new FeedParser()
 
-  // Define our handlers
-  req.on('response', function(res) {
-    if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'))
-    var encoding = res.headers['content-encoding'] || 'identity'
-      , charset = getParams(res.headers['content-type'] || '').charset
-    res = maybeDecompress(res, encoding)
-    res = maybeTranslate(res, charset)
-    res.pipe(feedparser)
+  // make sure we're getting the last redirect URL
+  var finalUrl = feed
+
+  var redirectReq = request(feed, {
+    method: 'HEAD',
+    followAllRedirects: true
+  }, (err, response, body) => {
+    console.log(response.request.href)
+    finalUrl = response.request.href
+
+    console.log(finalUrl)
+    var req = request(finalUrl, {
+      timeout: 10000,
+      pool: false
+    })
+
+    req.setMaxListeners(50)
+    // Some feeds do not respond without user-agent and accept headers.
+    req.setHeader('user-agent', 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36')
+    req.setHeader('accept', 'text/html,application/xhtml+xml')
+
+    // Define our handlers
+    req.on('response', function(res) {
+      if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'))
+      var encoding = res.headers['content-encoding'] || 'identity'
+        , charset = getParams(res.headers['content-type'] || '').charset
+      res = maybeDecompress(res, encoding)
+      res = maybeTranslate(res, charset)
+      res.pipe(feedparser)
+    })
   })
 
-  // feedparser.on('error', done)
+  feedparser.on('error', (error) => {
+    console.log(error)
+  })
+
   feedparser.on('end', () => {
     done(items)
   })
