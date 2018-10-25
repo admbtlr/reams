5823fe213fd39d8823ec0a83ec5d093035fa2bd8
adamvert <ab@adam-butler.com>
2018-10-25 19:22:41 +0200

    chore: remove sentry

--- app/ios/rizzle/AppDelegate.m
+++ app/ios/rizzle/AppDelegate.m
@@ -12,11 +12,6 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import "SplashScreen.h"
-#if __has_include(<React/RNSentry.h>)
-#import <React/RNSentry.h> // This is used for versions of react >= 0.40
-#else
-#import "RNSentry.h" // This is used for versions of react < 0.40
-#endif
 
 @implementation AppDelegate
 
@@ -39,9 +34,6 @@
                                                       moduleName:@"rizzle"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-
-  [RNSentry installWithRootView:rootView];
-
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
