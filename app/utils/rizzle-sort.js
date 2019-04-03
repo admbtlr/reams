1ba34f9bb27d67b428d3c6933307a368b579c152
adamvert <ab@adam-butler.com>
2019-04-03 17:01:07 +0200

    fix: improving rizzle-sort

--- app/utils/rizzle-sort.js
+++ app/utils/rizzle-sort.js
@@ -1,20 +1,24 @@
-export default function rizzleSort (items, feeds, shuffleStrength = 10) {
+export default function rizzleSort (items, feeds, shuffleStrength = 2) {
+  // sort by age
+  var now = Math.floor(Date.now() / 1000);
+  it.map(i => 1 / Math.floor((now - i.created_at) / 10000))
+
 // another possible algorithm here would be
 // sort the items by date
 // then sort each by day by feed reading_rate
-  const sorted = items.sort((a, b) => {
-    const aDate = new Date(a.created_at)
-    const bDate = new Date(b.created_at)
-    if (aDate.getFullYear() === bDate.getFullYear() &&
-      aDate.getMonth() === bDate.getMonth() &&
-      aDate.getDate() === bDate.getDate()) {
-      return (feeds.find(f => f._id === b.feed_id).reading_rate || 10) -
-        (feeds.find(f => f._id === a.feed_id).reading_rate || 10)
-    } else {
-      return b.created_at - a.created_at
-    }
-  })
-  return rizzleShuffle(sorted, shuffleStrength)
+  // const sorted = items.sort((a, b) => {
+  //   const aDate = new Date(a.created_at)
+  //   const bDate = new Date(b.created_at)
+  //   if (aDate.getFullYear() === bDate.getFullYear() &&
+  //     aDate.getMonth() === bDate.getMonth() &&
+  //     aDate.getDate() === bDate.getDate()) {
+  //     return (feeds.find(f => f._id === b.feed_id).reading_rate || 10) -
+  //       (feeds.find(f => f._id === a.feed_id).reading_rate || 10)
+  //   } else {
+  //     return b.created_at - a.created_at
+  //   }
+  // })
+  // return rizzleShuffle(sorted, shuffleStrength)
 
   // let itemsByFeed = {}
   // let keys = []
@@ -41,12 +45,12 @@ export default function rizzleSort (items, feeds, shuffleStrength = 10) {
 // adds a `shuffle_factor` to each item, so that the shuffle is deterministic
 function rizzleShuffle(items, shuffleStrength) {
   // this used to be (items.length / 10)
-  // console.log('Preshuffle: ' + items.map(i => i.title))
+  console.log('Preshuffle: ' + items.map(i => i.title))
   items = items.map(i => i.shuffle_factor
     ? i
     : {
       ...i,
-      shuffle_factor: Math.random() * shuffleStrength
+      shuffle_factor: Math.random() * shuffleStrength - shuffleStrength / 2
     })
 
   return items.sort((a, b) => (items.indexOf(a) - a.shuffle_factor) -
