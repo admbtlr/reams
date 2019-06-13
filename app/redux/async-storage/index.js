4c7a2d086bb52375dee39df23cf1ca6fedcfac4c
adamvert <ab@adam-butler.com>
2019-06-13 10:00:12 +0200

    chore: move to community async storage

--- app/redux/async-storage/index.js
+++ app/redux/async-storage/index.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
 import log from '../../utils/log'
 
 // export async function getItemAS (key) {
