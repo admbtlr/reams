b386bfdbb31e171a1065e0c62209b1d3ef7ac547
adamvert <ab@adam-butler.com>
2019-11-28 14:03:40 +0100

    fix: add user-agent to mercury request

--- server/server.js
+++ server/server.js
@@ -22,7 +22,11 @@ app.get('/unread/', (req, res) => {
 })
 
 app.get('/mercury/', (req, res) => {
-  Mercury.parse(req.query.url).then(parsed => {
+  Mercury.parse(req.query.url, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'
+    }
+  }).then(parsed => {
     res.send(parsed)
   }).catch(error => {
     res.send({
