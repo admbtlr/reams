f67993b3a88073bde1f06836d21b0e24aef61c7b
adamvert <ab@adam-butler.com>
2020-03-06 14:53:20 +0100

    fix: add timeout to request

--- server/services/site.ts
+++ server/services/site.ts
@@ -7,23 +7,25 @@ export async function findFeeds (url) {
     return
   }
   let homeUrl = matches[1]
-  console.log(`Checking ${homeUrl}`)
+  // console.log(`Checking ${homeUrl}`)
 
   try {
     // start with the current page
-    let res = await axios.get(url)
+    let res = await axios.get(url, {
+      timeout: 5000
+    })
     let body = res.data
     let linkTag: string 
     
     linkTag = checkForLinkToRssFile(body)
     if (linkTag) {
-      console.log('FOUND RSS URL IN A LINK: ' + linkTag)
+      // console.log('FOUND RSS URL IN A LINK: ' + linkTag)
       feeds.push(parseLinkTag(linkTag, homeUrl))
     }
 
     linkTag = checkForLinkWithRssInText(body)
     if (linkTag) {
-      console.log('FOUND RSS URL IN A LINK WITH RSS TEXT: ' + linkTag)
+      // console.log('FOUND RSS URL IN A LINK WITH RSS TEXT: ' + linkTag)
       feeds.push(parseLinkTag(linkTag, homeUrl))
     }
 
@@ -33,19 +35,19 @@ export async function findFeeds (url) {
 
     linkTag = checkForRSSHeader(body)
     if (linkTag) {
-      console.log('FOUND RSS URL IN HEADER: ' + linkTag)
+      // console.log('FOUND RSS URL IN HEADER: ' + linkTag)
       feeds.push(parseLinkTag(linkTag, homeUrl))
     }
 
     linkTag = checkForLinkToRssFile(body)
     if (linkTag) {
-      console.log('FOUND RSS URL IN A LINK: ' + linkTag)
+      // console.log('FOUND RSS URL IN A LINK: ' + linkTag)
       feeds.push(parseLinkTag(linkTag, homeUrl))
     }
 
     linkTag = checkForLinkWithRssInText(body)
     if (linkTag) {
-      console.log('FOUND RSS URL IN A LINK WITH RSS TEXT: ' + linkTag)
+      // console.log('FOUND RSS URL IN A LINK WITH RSS TEXT: ' + linkTag)
       feeds.push(parseLinkTag(linkTag, homeUrl))
     }
 
@@ -53,10 +55,10 @@ export async function findFeeds (url) {
       .filter((feed, index, self) => self.indexOf(feed) === index)
 
   } catch (error) {
-    console.log(`Error fetching page: ${error.message}`)
+    // console.log(`Error fetching page: ${error.message}`)
   } finally {
     let fullFeeds = []
-    console.log('Got feeds: ' + feeds)
+    // console.log('Got feeds: ' + feeds)
     for (let feed of feeds) {
       try {
         const res = await axios.get('https://api.rizzle.net/api/feed-title/?url=' + feed)
@@ -68,7 +70,7 @@ export async function findFeeds (url) {
           })
         }
       } catch (error) {
-        console.log('ERROR GETTING FEEDS: ' + error)
+        // console.log('ERROR GETTING FEEDS: ' + error)
       }
     }
     // console.log(fullFeeds)
@@ -86,28 +88,28 @@ function checkForRSSHeader (body: string) : string {
 }
 
 function checkForLinkToRssFile (body: string) : string {
-  console.log('Full body length: ' + body.length)
-  console.log('Now checking for links to rss files')
+  // console.log('Full body length: ' + body.length)
+  // console.log('Now checking for links to rss files')
   body = body.replace(/<script[^]*?<\/script>/mg, '')
   // console.log(body)
-  console.log('Scriptless body length: ' + body.length)
+  // console.log('Scriptless body length: ' + body.length)
   // look for links to rss files
   const now = Date.now()
   let matches: string[] = body.match(/<a.*?href.*?>/g)
   var regex = /<a.*?href.*?\.(rss|atom).*?>/
   matches = matches.filter(m => regex.test(m))
-  console.log(`Searching for links to RSS files took ${(Date.now() - now)}ms`)
+  // console.log(`Searching for links to RSS files took ${(Date.now() - now)}ms`)
   return (matches && matches.length > 0) ?
     matches[1] :
     null
 }
 
 function checkForLinkWithRssInText (body: string) : string {
-  console.log('Checking for link with RSS in text')
+  // console.log('Checking for link with RSS in text')
   body = body.replace(/<script[^]*?<\/script>/mg, '')
   const now = Date.now()
   const matches: string[] = body.match(/href[^>]*?>(rss|atom)/i)
-  console.log(`Searching for links with RSS in text took ${(Date.now() - now)}ms`)
+  // console.log(`Searching for links with RSS in text took ${(Date.now() - now)}ms`)
   return (matches && matches.length > 0 && matches[0]) ?
     matches[0] :
     null
@@ -118,14 +120,16 @@ async function checkKnownRssLocations (url: string) : Promise<string[]> {
   // Tumblr: /rss
   let feeds = []
   let matches = url.match(/(http[s]*:\/\/.+?(\/|$))(.*?(\/|$))/)
-  const host = matches[1]
+  const host = matches[1].charAt(matches[1].length - 1) === '/' ?
+    matches[1] :
+    matches[1] + '/'
   const subfolder = matches[3].length > 0 ?
     (matches[3].indexOf('/') === matches[3].length - 1 ?
       matches[3] :
       matches[3] + '/') :
     ''
-  console.log('URL: ' + url)
-  console.log('SUBFOLDER: ' + subfolder)
+  // console.log('URL: ' + url)
+  // console.log('SUBFOLDER: ' + subfolder)
 
   let feedUrl: string
   let res: Response
@@ -134,20 +138,20 @@ async function checkKnownRssLocations (url: string) : Promise<string[]> {
 
   // bent throws an error if it doesn't get a 200
   try {
-    console.log(`Checking ${host}feed`)
+    // console.log(`Checking ${host}feed`)
     await axios.get(feedUrl)
     feeds.push(feedUrl)
   } catch (error) {}
 
   try {
-    console.log(`Checking ${host}${subfolder}feed`)
+    // console.log(`Checking ${host}${subfolder}feed`)
     feedUrl = host + subfolder + 'feed'
     await axios.get(feedUrl)
     feeds.push(feedUrl)
   } catch (error) {}
 
   try {
-    console.log(`Checking ${host}rss`)
+    // console.log(`Checking ${host}rss`)
     feedUrl = host + 'rss'
     await axios.get(feedUrl)
     feeds.push(feedUrl)
@@ -159,11 +163,11 @@ async function checkKnownRssLocations (url: string) : Promise<string[]> {
 
 function parseLinkTag (linkTag: string, homeUrl: string) : string {
   let rssUrl = linkTag && linkTag.match(/href="(.*?)"/)[1]
-  console.log('MATCHING...')
+  // console.log('MATCHING...')
   if (!rssUrl.startsWith('http')) {
     rssUrl = (homeUrl + rssUrl)
     rssUrl = rssUrl.replace(/([^:])\/\//, '$1/')
   }
-  console.log(`Found an RSS feed: ${rssUrl}`)
+  // console.log(`Found an RSS feed: ${rssUrl}`)
   return rssUrl
 }
