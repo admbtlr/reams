414bd779519f77d9233ac8f0b167d5835c96ce5d
adamvert <ab@adam-butler.com>
2020-03-30 12:24:46 +0200

    fix: feed expansion again

--- app/components/TopBars.js
+++ app/components/TopBars.js
@@ -13,6 +13,10 @@ import {
 
 import TopBarContainer from '../containers/TopBar'
 import { isIphoneX } from '../utils'
+<<<<<<< HEAD
+=======
+import { getClampedScrollAnim } from '../utils/animation-handlers'
+>>>>>>> fix: feed expansion again
 
 export const STATUS_BAR_HEIGHT = 70 + (isIphoneX() ? 44 : 22)
 
