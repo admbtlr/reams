5ef87a4ca300d3358aa08986b96a16feba24c66b
adamvert <ab@adam-butler.com>
2020-08-28 17:12:14 +0200

    feat: separate bundles for share ext!

--- app/ios/RizzleShare/RizzleShare.m
+++ app/ios/RizzleShare/RizzleShare.m
@@ -3,7 +3,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <React/RCTLog.h>
-#import <Firebase.h>
+//#import <Firebase.h>
 
 @interface RizzleShare : ReactNativeShareExtension
 @end
@@ -15,7 +15,7 @@ RCT_EXPORT_MODULE();
 - (UIView*) shareView {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.share" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"RizzleShare"
@@ -26,9 +26,9 @@ RCT_EXPORT_MODULE();
   // Uncomment for console output in Xcode console for release mode on device:
   // RCTSetLogThreshold(RCTLogLevelInfo - 1);
 
-  if ([FIRApp defaultApp] == nil) {
-    [FIRApp configure];
-  }
+//  if ([FIRApp defaultApp] == nil) {
+//    [FIRApp configure];
+//  }
 
   return rootView;
 }
