d1bd0c0743bd61596c93ea5f342d2a19b1dbfa58
Adam Butler <ab@adam-butler.com>
2022-11-03 11:16:39 +0100

    chore: migrate from unimodules to expo
    Squashed commit of the following:
    
    commit e15b4752ec177ff493e5328610d8317181604d03
    Author: Adam Butler <ab@adam-butler.com>
    Date:   Thu Nov 3 11:15:56 2022 +0100
    
        move to expo

--- app/ios/rizzle/AppDelegate.m
+++ app/ios/rizzle/AppDelegate.m
@@ -4,14 +4,11 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <React/RCTLinkingManager.h>
+#import <React/RCTConvert.h>
 #import "RNSplashScreen.h"
 
 #import <TSBackgroundFetch/TSBackgroundFetch.h>
 
-#import <UMCore/UMModuleRegistry.h>
-#import <UMReactNativeAdapter/UMNativeModulesProxy.h>
-#import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
- 
 #ifdef FB_SONARKIT_ENABLED
 // #import <FlipperKit/FlipperClient.h>
 // #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -30,12 +27,6 @@
 // }
 #endif
 
-@interface AppDelegate () <RCTBridgeDelegate>
- 
-@property (nonatomic, strong) UMModuleRegistryAdapter *moduleRegistryAdapter;
- 
-@end
- 
 @implementation AppDelegate
 
 // These two blocks are for Universal Links
@@ -59,8 +50,6 @@
 //   InitializeFlipper(application);
 // #endif
 
-  self.moduleRegistryAdapter = [[UMModuleRegistryAdapter alloc] initWithModuleRegistryProvider:[[UMModuleRegistryProvider alloc] init]];
- 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                               moduleName:@"rizzle"
@@ -100,9 +89,8 @@
 
 - (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge
 {
-    NSArray<id<RCTBridgeModule>> *extraModules = [_moduleRegistryAdapter extraModulesForBridge:bridge];
     // If you'd like to export some custom RCTBridgeModules that are not Expo modules, add them here!
-    return extraModules;
+    return @[];
 }
  
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
