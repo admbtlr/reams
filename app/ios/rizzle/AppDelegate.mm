50f93a0edaa0c9bd582666b554af70ad8e2a2ec4
Adam Butler <ab@adam-butler.com>
2022-11-11 20:50:27 +0100

    chore: rn 0.68.2 & expo 45 🙌
    commit 61c8b1a266d443d1bec116893162a260760aa0a6
    Author: Adam Butler <ab@adam-butler.com>
    Date:   Fri Nov 11 20:49:24 2022 +0100
    
        fixes
    
    commit cd068bb461f8c2eecb00d7ef681c51d6a252eb5c
    Author: Adam Butler <ab@adam-butler.com>
    Date:   Fri Nov 11 19:10:15 2022 +0100
    
        expo 45
    
    commit 866368ddf48b2d09a311105f279caf51b11d82fc
    Author: Adam Butler <ab@adam-butler.com>
    Date:   Fri Nov 11 15:14:55 2022 +0100
    
        oops
    
    commit 7b391771db0a8738b52acfbe32cea050704987c0
    Author: Adam Butler <ab@adam-butler.com>
    Date:   Fri Nov 11 15:06:10 2022 +0100
    
        do the diffs

--- app/ios/rizzle/AppDelegate.mm
+++ app/ios/rizzle/AppDelegate.mm
@@ -9,22 +9,25 @@
 
 #import <TSBackgroundFetch/TSBackgroundFetch.h>
 
-#ifdef FB_SONARKIT_ENABLED
-// #import <FlipperKit/FlipperClient.h>
-// #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-// #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-// #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-// #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-// #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-// static void InitializeFlipper(UIApplication *application) {
-//   FlipperClient *client = [FlipperClient sharedClient];
-//   SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-//   [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
-//   [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
-//   [client addPlugin:[FlipperKitReactPlugin new]];
-//   [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
-//   [client start];
-// }
+#import <React/RCTAppSetupUtils.h>
+
+#if RCT_NEW_ARCH_ENABLED
+#import <React/CoreModulesPlugins.h>
+#import <React/RCTCxxBridgeDelegate.h>
+#import <React/RCTFabricSurfaceHostingProxyRootView.h>
+#import <React/RCTSurfacePresenter.h>
+#import <React/RCTSurfacePresenterBridgeAdapter.h>
+#import <ReactCommon/RCTTurboModuleManager.h>
+
+#import <react/config/ReactNativeConfig.h>
+
+@interface AppDelegate () <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
+  RCTTurboModuleManager *_turboModuleManager;
+  RCTSurfacePresenterBridgeAdapter *_bridgeAdapter;
+  std::shared_ptr<const facebook::react::ReactNativeConfig> _reactNativeConfig;
+  facebook::react::ContextContainer::Shared _contextContainer;
+}
+@end
 #endif
 
 @implementation AppDelegate
@@ -46,16 +49,25 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-// #ifdef FB_SONARKIT_ENABLED
-//   InitializeFlipper(application);
-// #endif
+  RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [self.reactDelegate createBridgeWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [self.reactDelegate createRootViewWithBridge:bridge moduleName:@"rizzle" initialProperties:nil];                                              
+
+#if RCT_NEW_ARCH_ENABLED
+  _contextContainer = std::make_shared<facebook::react::ContextContainer const>();
+  _reactNativeConfig = std::make_shared<facebook::react::EmptyReactNativeConfig const>();
+  _contextContainer->insert("ReactNativeConfig", _reactNativeConfig);
+  _bridgeAdapter = [[RCTSurfacePresenterBridgeAdapter alloc] initWithBridge:bridge contextContainer:_contextContainer];
+  bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
+#endif
+
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"rizzle", nil);
 
   // rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  // new but probably wrong for exmp:
+  // UIViewController *rootViewController = [UIViewController new];
   UIViewController *rootViewController = [self.reactDelegate createRootViewController];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
@@ -94,7 +106,7 @@
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
@@ -104,3 +116,42 @@
 
 
 @end
+
+#if RCT_NEW_ARCH_ENABLED
+
+#pragma mark - RCTCxxBridgeDelegate
+
+- (std::unique_ptr<facebook::react::JSExecutorFactory>)jsExecutorFactoryForBridge:(RCTBridge *)bridge
+{
+  _turboModuleManager = [[RCTTurboModuleManager alloc] initWithBridge:bridge
+                                                             delegate:self
+                                                            jsInvoker:bridge.jsCallInvoker];
+  return RCTAppSetupDefaultJsExecutorFactory(bridge, _turboModuleManager);
+}
+
+#pragma mark RCTTurboModuleManagerDelegate
+
+- (Class)getModuleClassFromName:(const char *)name
+{
+  return RCTCoreModulesClassProvider(name);
+}
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
+                                                      jsInvoker:(std::shared_ptr<facebook::react::CallInvoker>)jsInvoker
+{
+  return nullptr;
+}
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
+                                                     initParams:
+                                                         (const facebook::react::ObjCTurboModule::InitParams &)params
+{
+  return nullptr;
+}
+
+- (id<RCTTurboModule>)getModuleInstanceFromClass:(Class)moduleClass
+{
+  return RCTAppSetupDefaultModuleFromClass(moduleClass);
+}
+
+#endif
