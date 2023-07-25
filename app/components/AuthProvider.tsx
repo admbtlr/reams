2f16da5250ee4124f1b3def3c9baf4f93d9fe5b8
Adam Butler <ab@adam-butler.com>
2023-07-25 10:38:07 +0200

    fix: authListener.unsubscribe is not a function

--- app/components/AuthProvider.tsx
+++ app/components/AuthProvider.tsx
@@ -35,7 +35,7 @@ export const AuthProvider = (props: any) => {
       }
     )
     return () => {
-      authListener!.unsubscribe()
+      authListener!.subscription.unsubscribe()
     }
   }, [])
 
