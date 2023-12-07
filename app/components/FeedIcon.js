5d04559388e032fe3c5100f9d611df53d65269e4
Adam Butler <ab@adam-butler.com>
2023-12-07 15:31:21 +0100

    chore: FeedIcon handles icon caching

--- app/components/FeedIcon.js
+++ app/components/FeedIcon.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import * as FileSystem from 'expo-file-system'
 import {
   Image,
   Platform,
   View
 } from 'react-native'
-import {getCachedFeedIconPath} from '../utils/'
+import {fileExists, getCachedFeedIconPath} from '../utils/'
 
 function FeedIcon ({
   feed,
@@ -14,19 +15,44 @@ function FeedIcon ({
   isSmall,
   isSmaller
 }) {
+  const [ isCached, setIsCached ] = React.useState()
+
+  React.useEffect(() => {
+    if (feed && Platform.OS !== 'web') {
+      const path = getCachedFeedIconPath(feed._id)
+      fileExists(path).
+        then(setIsCached).
+        catch(() => setIsCached(false))
+    }
+  }, [])
+
+  React.useEffect(() => {
+    if (!isCached && feed) {
+      FileSystem.
+        downloadAsync(feed.favicon.url, getCachedFeedIconPath(feed._id)).
+        then(() => setIsCached(true)).
+        catch(() => setIsCached(false))
+    }
+  }, [ isCached ])
+
+  if (isCached === undefined) {
+    return null
+  }
+
   const width = isSmaller ? 18 : isSmall ? 24 : 32
   const height = isSmaller ? 18 : isSmall ? 24 : 32
   let dim = dimensions || iconDimensions
   const image = feed ? <Image
     width={width}
     height={height}
-    source={{ uri: Platform.OS === 'web' ? feed.favicon?.url : getCachedFeedIconPath(feed._id) }}
+    // source={{ uri: Platform.OS === 'web' ? feed.favicon?.url : getCachedFeedIconPath(feed._id) }}
+    source={{ uri: isCached ? getCachedFeedIconPath(feed._id) : feed.favicon?.url }}
     style={{
       width,
       height,
     }}
   /> : null
-  return (Platform.OS === 'web' && feed?.favicon?.url) || (hasCachedIcon && dim && dim.width > 0) ?
+  return feed?.favicon?.url || (isCached && dim && dim.width > 0) ?
     <View style={{
       backgroundColor: feed ? feed.color : 'white',
       // margin: 10,
