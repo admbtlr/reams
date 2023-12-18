02b88669e5d7032be94080b9d74e440d3e031377
Adam Butler <ab@adam-butler.com>
2023-12-18 17:25:30 +0100

    chore: unirepo

--- server/api/find-feeds.ts
+++
@@ -1,9 +0,0 @@
-import { NowRequest, NowResponse } from '@now/node'
-import { findFeeds } from '../services/site'
-
-export default async (req: NowRequest, res: NowResponse) => {
-  const siteUrl = req.query.url || 'http://localhost:3000/api/headers'
-  const extended = !!req.query.extended
-  const feeds = await findFeeds(siteUrl, extended)
-  res.json(feeds)
-}
