17490514491334949a4eae7d8b9e7f78eefc2c76
Adam Butler <ab@adam-butler.com>
2023-12-21 17:45:45 +0100

    fix: merge decorate-items with master

--- app/sagas/decorate-items.ts
+++ app/sagas/decorate-items.ts
@@ -4,7 +4,13 @@ import * as FileSystem from 'expo-file-system'
 import { InteractionManager, Platform } from 'react-native'
 import { 
   ITEM_DECORATION_FAILURE,
-  ITEM_DECORATION_SUCCESS
+  ITEM_DECORATION_SUCCESS,
+  ImageStuff,
+  Item,
+  ItemInflated,
+  ItemType,
+  MercuryStuff,
+  UPDATE_ITEM
 } from '../store/items/types'
 import { getCachedCoverImagePath, getImageDimensions } from '../utils'
 import { setCoverInline, setCoverAlign, setTitleVAlign } from '../utils/createItemStyles'
@@ -27,22 +33,33 @@ import {
   getItems as getItemsIDB, 
   updateItem as updateItemIDB
 } from '../storage/idb-storage'
-import { persistDecoration } from './update-item'
+import { Feed } from '../store/feeds/types'
+import { Category } from '../store/categories/types'
+import { RootState } from '../store/reducers'
+import { Filter } from '../store/config/config'
+import { addCoverImageToItem, addMercuryStuffToItem, deflateItem, removeCachedCoverImageDuplicate, setShowCoverImage } from '../utils/item-utils'
 
-
-let pendingDecoration = [] // a local cache
+let pendingDecoration: Item[] = [] // a local cache
 let toDispatch = []
 
 const showLogs = true
 
-export function * decorateItems (action) {
+interface Decoration {
+  item: WholeItem
+  mercuryStuff: MercuryStuff
+  imageStuff: ImageStuff
+}
+
+interface WholeItem extends Item, ItemInflated {}
+
+export function * decorateItems () {
   let items
   let item
   let count = 0
 
   yield spawn(function * () {
     while (true) {
-      const nextItem = yield getNextItemToDecorate()
+      const nextItem: Item = yield getNextItemToDecorate()
       // console.log('Looking for new item')
       if (nextItem) {
         consoleLog(`Got item to decorate: ${nextItem.title}`)
@@ -97,7 +114,9 @@ function consoleLog(txt: string) {
 }
 
 export function * assembleDecoration (i: Item): Generator<any, Decoration | boolean, any> {
-  let items: ItemInflated[] = yield call(getItemsSQLite, [i])
+  let items: ItemInflated[] = Platform.OS === 'web' ?
+    yield call(getItemsIDB, [i]) :
+    yield call(getItemsSQLite, [i])
   let itemInflated = items[0]
   let item: WholeItem = { ...i , ...itemInflated}
   const mercuryStuff: MercuryStuff = yield call(loadMercuryStuff, item)
@@ -112,9 +131,10 @@ export function * assembleDecoration (i: Item): Generator<any, Decoration | bool
   }
 }
 
-function * applyDecoration (decoration: Decoration, isSaved: boolean = false) {
+function * applyDecoration (decoration: Decoration) {
   yield call(InteractionManager.runAfterInteractions)
-  const displayMode = yield select(getDisplay)
+  const displayMode: string = yield select(getDisplay)
+  const isSaved = decoration.item.isSaved
   yield call(persistDecoration, {...decoration})
   yield put({
     type: ITEM_DECORATION_SUCCESS,
@@ -122,16 +142,7 @@ function * applyDecoration (decoration: Decoration, isSaved: boolean = false) {
     isSaved,
     displayMode
   })
-  const items = [ ...yield select(getItems) ]
-  decoratedCount = items.filter((item) => item.isDecorated).length
-  // consoleLog(`DECORATED ${decoratedCount} OUT OF ${items.length}`)
-
-  yield call(InteractionManager.runAfterInteractions)
-  yield put({
-    type: 'ITEM_DECORATION_PROGRESS',
-    totalCount: items.length,
-    decoratedCount
-  })
+  const items: Item[] = [ ...yield select(getItems, isSaved ? ItemType.saved : ItemType.unread) ]
 
   let item = items.find(item => item._id === decoration.item._id)
   if (item) {
@@ -150,18 +161,107 @@ function * applyDecoration (decoration: Decoration, isSaved: boolean = false) {
   }
 }
 
+function * persistDecoration (decoration: Decoration) {
+  const {imageStuff, item, mercuryStuff} = decoration
+  const decorated = addMercuryStuffToItem(item, mercuryStuff)
+  let wholeItem = {
+    ...item,
+    ...decorated
+  }
+  wholeItem = addCoverImageToItem(wholeItem, imageStuff)
+  wholeItem.hasCoverImage = !!wholeItem.coverImageFile
+  wholeItem = setShowCoverImage(wholeItem)
+  wholeItem = removeCachedCoverImageDuplicate(wholeItem)
+  wholeItem.styles = adjustStylesToCoverImage(decoration)
+  if (Platform.OS === 'web') {
+    yield call(updateItemIDB, wholeItem)
+  } else {
+    yield call(updateItemSQLite, wholeItem)
+  }
+  yield put({ 
+    type: UPDATE_ITEM,
+    item: deflateItem(wholeItem)
+  })
+}
+
+function * prepareCoverImage (item: Item, mercuryStuff: MercuryStuff): Generator<any, ImageStuff, any> {
+  let imageStuff = {}
+  if (mercuryStuff.lead_image_url && Platform.OS !== 'web') {
+    let coverImageFile = yield call (cacheCoverImage, item, mercuryStuff.lead_image_url)
+    if (coverImageFile) {
+      try {
+        const imageDimensions = yield call(getImageDimensions, getCachedCoverImagePath(item))
+        // const faceCentreNormalised = yield call(faceDetection, coverImageFile, imageDimensions)
+        imageStuff = {
+          coverImageFile,
+          imageDimensions,
+          // faceCentreNormalised
+        }
+      } catch (error: any) {
+        consoleLog(error)
+      }
+    }
+  }
+  return imageStuff
+}
+
+function adjustStylesToCoverImage (decoration: Decoration): {} {
+  let { imageStuff, item, mercuryStuff } = decoration
+  let styles = { ...item.styles }
+  if (imageStuff.faceCentreNormalised) {
+    const { x, y } = imageStuff.faceCentreNormalised
+    const hAlign = x < 0.333 ? 'left' :
+      x < 0.666 ? 'center' : 'right'
+    const vAlign = y < 0.5 ? 'bottom' : 'top'
+    styles = setCoverAlign(hAlign, styles)
+    styles = setTitleVAlign(vAlign, styles)
+  }
+
+  if (!!item.title &&
+    ((
+      Math.random() > 0.5 /*&&
+      imageStuff.imageDimensions?.height < imageStuff.imageDimensions?.width / 1.8*/
+    ) || mercuryStuff.excerpt && mercuryStuff.excerpt.length > 180)) {
+    if (styles) {
+      styles = setCoverInline(styles)
+    }
+    item.showCoverImage = true
+  }
+  return styles
+}
+
+export async function cacheCoverImage (item, imageURL) {
+  const splitted = imageURL.split('.')
+  // const extension = splitted[splitted.length - 1].split('?')[0].split('%')[0]
+  // making a big assumption on the .jpg extension here...
+  // and it seems like Image adds '.png' to a filename if there's no extension
+  const fileName = getCachedCoverImagePath(item)
+  // consoleLog(`Loading cover image for ${item._id}...`)
+  const fileInfo = await FileSystem.getInfoAsync(fileName)
+  if (fileInfo.exists) return fileName
+  try {
+    await FileSystem.downloadAsync(imageURL, fileName)
+    return fileName
+  } catch(err) {
+    consoleLog(`Loading cover image for ${item._id} failed :(`)
+    consoleLog(err)
+    return false
+  }
+}
+
 function * getNextItemToDecorate () {
   let nextItem
-  const savedItems = yield select(getSavedItems)
+  const savedItems: Item[] = yield select(getSavedItems)
   nextItem = savedItems.find(item => item.title === 'Loading...' &&
-    (!item.decoration_failures || item.decoration_failures < 5))
+    (!item.decoration_failures || item.decoration_failures < 5) &&
+    !item.isExternal) // external items handle their own decoration
   if (nextItem) return nextItem
 
-  const items = yield select(getItems)
-  const index = yield select(getIndex)
-  const feeds = yield select(getFeeds)
-  const categories = yield select(state => state.categories.categories)
-  const filter = yield select(state => state.config.filter)
+  const items: Item[] = yield select(getItems, ItemType.unread)
+  const index: number = yield select(getIndex, ItemType.unread)
+  const feeds: Feed[] = yield select(getFeeds)
+  const categories: Category[] = yield select(state => state.categories.categories)
+  const filter: Filter = yield select((state: RootState) => state.config.filter)
   const activeFilter = filter?._id ? categories.find(c => c._id === filter._id) : null
   const feedsWithoutDecoration = feeds.filter(feed => {
     // external items handle their own decoration
@@ -169,7 +269,7 @@ function * getNextItemToDecorate () {
       .find(item => typeof item.coverImageUrl !== 'undefined')
   })
   let count = 0
-  let feed
+  let feed: Feed
   const candidateItems = items.filter(item => {
     return !item.isDecorated &&
       (!item.decoration_failures || item.decoration_failures < 3) &&
@@ -194,88 +294,10 @@ function * getNextItemToDecorate () {
   }
   if (!nextItem) {
     nextItem = savedItems.find(item => !item.isDecorated &&
+      item.decoration_failures &&
       item.decoration_failures < 5 &&
       !pendingDecoration.find(pd => pd._id === item._id))
   }
   return nextItem
 }
 
-export function * decorateItem (i) {
-  let item = { ...i }
-  let imageStuff = {}
-  // external items come through here before they have any styles
-item.styles = item.styles ? { ...item.styles } : {}
-  let items = Platform.OS === 'web' ?
-    yield call(getItemsIDB, [item]) :
-    yield call (getItemsSQLite, [item])
-  item = items.length ? items[0] : item
-  const mercuryStuff = yield call(loadMercuryStuff, item)
-  if (!mercuryStuff) {
-    return false
-  }
-
-  // consoleLog(`Loading Mercury stuff for ${item._id} done`)
-  if (mercuryStuff.lead_image_url) {
-    if (Platform.OS !== 'web') {
-      let coverImageFile = yield call (cacheCoverImage, item, mercuryStuff.lead_image_url)
-      if (coverImageFile) {
-        try {
-          const imageDimensions = yield call(getImageDimensions, getCachedCoverImagePath(item))
-          const faceCentreNormalised = yield call(faceDetection, coverImageFile, imageDimensions)
-          imageStuff = {
-            coverImageFile,
-            imageDimensions,
-            faceCentreNormalised
-          }
-        } catch (error) {
-          consoleLog(error)
-        }
-      }
-      if (imageStuff.faceCentreNormalised) {
-        const { x, y } = imageStuff.faceCentreNormalised
-        const hAlign = x < 0.333 ? 'left' :
-          x < 0.666 ? 'center' : 'right'
-        const vAlign = y < 0.5 ? 'bottom' : 'top'
-        setCoverAlign(hAlign, item.styles)
-        setTitleVAlign(vAlign, item.styles)
-      }
-    }
-
-    if (!!item.title &&
-      ((
-        Math.random() > 0.5 /*&&
-        imageStuff.imageDimensions?.height < imageStuff.imageDimensions?.width / 1.8*/
-      ) || mercuryStuff.excerpt && mercuryStuff.excerpt.length > 180)) {
-      if (item.styles) {
-        item.styles = setCoverInline(item.styles)
-      }
-      item.showCoverImage = true
-    }
-  }
-
-
-  return {
-    item,
-    mercuryStuff,
-    imageStuff
-  }
-}
-
-export async function cacheCoverImage (item, imageURL) {
-  const splitted = imageURL.split('.')
-  // const extension = splitted[splitted.length - 1].split('?')[0].split('%')[0]
-  // making a big assumption on the .jpg extension here...
-  // and it seems like Image adds '.png' to a filename if there's no extension
-  const fileName = getCachedCoverImagePath(item)
-  // consoleLog(`Loading cover image for ${item._id}...`)
-  const fileInfo = await FileSystem.getInfoAsync(fileName)
-  if (fileInfo.exists) return fileName
-  try {
-    await FileSystem.downloadAsync(imageURL, fileName)
-    return fileName
-  } catch(err) {
-    consoleLog(`Loading cover image for ${item._id} failed :(`)
-    consoleLog(err)
-    return false
-  }
-}
