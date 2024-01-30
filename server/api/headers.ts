5680e5bd0f1f66f898a1027fe4388bc49c545d12
Adam Butler <ab@adam-butler.com>
2024-01-30 17:58:34 +0100

    chore: remove server

--- server/api/headers.ts
+++
@@ -1,6 +0,0 @@
-import { NowRequest, NowResponse } from '@now/node'
-
-export default async (req: NowRequest, res: NowResponse) => {
-  console.log(req.headers)
-  res.send(req.headers)
-}
--- server/api/test.ts
+++
@@ -1,8 +0,0 @@
-import { NowRequest, NowResponse } from '@now/node'
-const feedService = require('../services/feed.js')
-
-export default async (req: NowRequest, res: NowResponse) => {
-  const feedUrl = req.query.url || 'https://www.theguardian.com/world/rss'
-  const titleDesc = await feedService.getTitleAndDescription(feedUrl)
-  res.json(titleDesc)
-}
