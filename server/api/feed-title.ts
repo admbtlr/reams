169b7b01c1e5960b024e65d28fbc5e4e27bff905
Adam Butler <ab@adam-butler.com>
2024-01-30 17:58:34 +0100

    chore: remove server

--- server/api/feed-title.ts
+++
@@ -1,8 +0,0 @@
-import { NowRequest, NowResponse } from '@now/node'
-import { getTitleAndDescription } from '../services/feed'
-
-export default async (req: NowRequest, res: NowResponse) => {
-  const feedUrl = req.query.url || 'https://www.theguardian.com/world/rss'
-  const titleDesc = await getTitleAndDescription(feedUrl)
-  res.json(titleDesc)
-}
