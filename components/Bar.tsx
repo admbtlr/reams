9e9d52e8867f00008d22865b8152385f8100d459
Adam Butler <ab@adam-butler.com>
2024-10-04 12:36:41 +0200

    chore: add type

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { View } from "react-native"
 import { useColor } from "../hooks/useColor"
 import { getMargin } from '../utils/dimensions'
+import { Item } from '../store/items/types'
 
-
-export const Bar = ({ item }) => {
+export const Bar = ({ item }: { item: Item }) => {
   const color = useColor(item.url)
   return <View style={{
     height: 20,
