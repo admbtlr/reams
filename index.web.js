82b63d8711ab9aa270d619da905229d364ceca6b
Adam Butler <ab@adam-butler.com>
2024-11-14 16:56:21 +0100

    fix(web): custom fonts

--- index.web.js
+++ index.web.js
@@ -62,132 +62,132 @@ import './public/fonts/Poppins-Regular.ttf'
 // import './web/fonts/fonts.css'
 
 const IBMPlexMonoBold = `@font-face { 
-  src: url('./public/fonts/IBMPlexMono-Bold.ttf')
-  font-family: 'IBMPlexMono-Bold'
+  src: url('/public/fonts/IBMPlexMono-Bold.ttf');
+  font-family: 'IBMPlexMono-Bold';
 }`
 const IBMPlexMonoBoldItalic = `@font-face { 
-  src: url('./public/fonts/IBMPlexMono-BoldItalic.ttf')
-  font-family: 'IBMPlexMono-BoldItalic'
+  src: url('/public/fonts/IBMPlexMono-BoldItalic.ttf');
+  font-family: 'IBMPlexMono-BoldItalic';
 }`
 const IBMPlexMonoItalic = `@font-face { 
-  src: url('./public/fonts/IBMPlexMono-Italic.ttf')
-  font-family: 'IBMPlexMono-Italic'
+  src: url('/public/fonts/IBMPlexMono-Italic.ttf');
+  font-family: 'IBMPlexMono-Italic';
 }`
 const IBMPlexMonoLight = `@font-face { 
-  src: url('./public/fonts/IBMPlexMono-Light.ttf')
-  font-family: 'IBMPlexMono-Light'
+  src: url('/public/fonts/IBMPlexMono-Light.ttf');
+  font-family: 'IBMPlexMono-Light';
 }`
 const IBMPlexMonoLightItalic = `@font-face { 
-  src: url('./public/fonts/IBMPlexMono-LightItalic.ttf')
-  font-family: 'IBMPlexMono-LightItalic'
+  src: url('/public/fonts/IBMPlexMono-LightItalic.ttf');
+  font-family: 'IBMPlexMono-LightItalic';
 }`
 const IBMPlexMono = `@font-face { 
-  src: url('./public/fonts/IBMPlexMono.ttf')
-  font-family: 'fonts/IBMPlexMono'
+  src: url('/public/fonts/IBMPlexMono.ttf');
+  font-family: 'IBMPlexMono';
 }`
 const IBMPlexSansBold = `@font-face { 
-  src: url('./public/fonts/IBMPlexSans-Bold.ttf')
-  font-family: 'IBMPlexSans-Bold'
+  src: url('/public/fonts/IBMPlexSans-Bold.ttf');
+  font-family: 'IBMPlexSans-Bold';
 }`
 const IBMPlexSansBoldItalic = `@font-face { 
-  src: url('./public/fonts/IBMPlexSans-BoldItalic.ttf')
-  font-family: 'IBMPlexSans-BoldItalic'
+  src: url('/public/fonts/IBMPlexSans-BoldItalic.ttf');
+  font-family: 'IBMPlexSans-BoldItalic';
 }`
 const IBMPlexSansItalic = `@font-face { 
-  src: url('./public/fonts/IBMPlexSans-Italic.ttf')
-  font-family: 'IBMPlexSans-Italic'
+  src: url('/public/fonts/IBMPlexSans-Italic.ttf');
+  font-family: 'IBMPlexSans-Italic';
 }`
 const IBMPlexSansLight = `@font-face { 
-  src: url('./public/fonts/IBMPlexSans-Light.ttf')
-  font-family: 'IBMPlexSans-Light'
+  src: url('/public/fonts/IBMPlexSans-Light.ttf');
+  font-family: 'IBMPlexSans-Light';
 }`
 const IBMPlexSansLightItalic = `@font-face { 
-  src: url('./public/fonts/IBMPlexSans-LightItalic.ttf')
-  font-family: 'IBMPlexSans-LightItalic'
+  src: url('/public/fonts/IBMPlexSans-LightItalic.ttf');
+  font-family: 'IBMPlexSans-LightItalic';
 }`
 const IBMPlexSans = `@font-face { 
-  src: url('./public/fonts/IBMPlexSans.ttf')
-  font-family: 'fonts/IBMPlexSans'
+  src: url('/public/fonts/IBMPlexSans.ttf');
+  font-family: 'IBMPlexSans';
 }`
 const IBMPlexSansCondBold = `@font-face { 
-  src: url('./public/fonts/IBMPlexSansCond-Bold.ttf')
-  font-family: 'IBMPlexSansCond-Bold'
+  src: url('/public/fonts/IBMPlexSansCond-Bold.ttf');
+  font-family: 'IBMPlexSansCond-Bold';
 }`
 const IBMPlexSansCondBoldItalic = `@font-face { 
-  src: url('./public/fonts/IBMPlexSansCond-BoldItalic.ttf')
-  font-family: 'IBMPlexSansCond-BoldItalic'
+  src: url('/public/fonts/IBMPlexSansCond-BoldItalic.ttf');
+  font-family: 'IBMPlexSansCond-BoldItalic';
 }`
 const IBMPlexSansCondExtraLight = `@font-face { 
-  src: url('./public/fonts/IBMPlexSansCond-ExtraLight.ttf')
-  font-family: 'IBMPlexSansCond-ExtraLight'
+  src: url('/public/fonts/IBMPlexSansCond-ExtraLight.ttf');
+  font-family: 'IBMPlexSansCond-ExtraLight';
 }`
 const IBMPlexSansCondExtraLightItalic = `@font-face { 
-  src: url('./public/fonts/IBMPlexSansCond-ExtraLightItalic.ttf')
-  font-family: 'IBMPlexSansCond-ExtraLightItalic'
+  src: url('/public/fonts/IBMPlexSansCond-ExtraLightItalic.ttf');
+  font-family: 'IBMPlexSansCond-ExtraLightItalic';
 }`
 const IBMPlexSansCond = `@font-face { 
-  src: url('./public/fonts/IBMPlexSansCond.ttf')
-  font-family: 'fonts/IBMPlexSansCond'
+  src: url('/public/fonts/IBMPlexSansCond.ttf');
+  font-family: 'IBMPlexSansCond';
 }`
 const IBMPlexSerifBold = `@font-face { 
-  src: url('./public/fonts/IBMPlexSerif-Bold.ttf')
-  font-family: 'IBMPlexSerif-Bold'
+  src: url('/public/fonts/IBMPlexSerif-Bold.ttf');
+  font-family: 'IBMPlexSerif-Bold';
 }`
 const IBMPlexSerifBoldItalic = `@font-face { 
-  src: url('./public/fonts/IBMPlexSerif-BoldItalic.ttf')
-  font-family: 'IBMPlexSerif-BoldItalic'
+  src: url('/public/fonts/IBMPlexSerif-BoldItalic.ttf');
+  font-family: 'IBMPlexSerif-BoldItalic';
 }`
 const IBMPlexSerifItalic = `@font-face { 
-  src: url('./public/fonts/IBMPlexSerif-Italic.ttf')
-  font-family: 'IBMPlexSerif-Italic'
+  src: url('/public/fonts/IBMPlexSerif-Italic.ttf');
+  font-family: 'IBMPlexSerif-Italic';
 }`
 const IBMPlexSerifLight = `@font-face { 
-  src: url('./public/fonts/IBMPlexSerif-Light.ttf')
-  font-family: 'IBMPlexSerif-Light'
+  src: url('/public/fonts/IBMPlexSerif-Light.ttf');
+  font-family: 'IBMPlexSerif-Light';
 }`
 const IBMPlexSerifLightItalic = `@font-face { 
-  src: url('./public/fonts/IBMPlexSerif-LightItalic.ttf')
-  font-family: 'IBMPlexSerif-LightItalic'
+  src: url('/public/fonts/IBMPlexSerif-LightItalic.ttf');
+  font-family: 'IBMPlexSerif-LightItalic';
 }`
 const IBMPlexSerifThin = `@font-face { 
-  src: url('./public/fonts/IBMPlexSerif-Thin.ttf')
-  font-family: 'IBMPlexSerif-Thin'
+  src: url('/public/fonts/IBMPlexSerif-Thin.ttf');
+  font-family: 'IBMPlexSerif-Thin';
 }`
 const IBMPlexSerifThinItalic = `@font-face { 
-  src: url('./public/fonts/IBMPlexSerif-ThinItalic.ttf')
-  font-family: 'IBMPlexSerif-ThinItalic'
+  src: url('/public/fonts/IBMPlexSerif-ThinItalic.ttf');
+  font-family: 'IBMPlexSerif-ThinItalic';
 }`
 const IBMPlexSerif = `@font-face { 
-  src: url('./public/fonts/IBMPlexSerif.ttf')
-  font-family: 'fonts/IBMPlexSerif'
+  src: url('/public/fonts/IBMPlexSerif.ttf');
+  font-family: 'IBMPlexSerif';
 }`
 const LibreBaskervilleBold = `@font-face { 
-  src: url('./public/fonts/LibreBaskerville-Bold.ttf')
-  font-family: 'LibreBaskerville-Bold'
+  src: url('/public/fonts/LibreBaskerville-Bold.ttf');
+  font-family: 'LibreBaskerville-Bold';
 }`
 const LibreBaskervilleItalic = `@font-face { 
-  src: url('./public/fonts/LibreBaskerville-Italic.ttf')
-  font-family: 'LibreBaskerville-Italic'
+  src: url('/public/fonts/LibreBaskerville-Italic.ttf');
+  font-family: 'LibreBaskerville-Italic';
 }`
 const LibreBaskervilleRegular = `@font-face { 
-  src: url('./public/fonts/LibreBaskerville-Regular.ttf')
-  font-family: 'LibreBaskerville-Regular'
+  src: url('/public/fonts/LibreBaskerville-Regular.ttf');
+  font-family: 'LibreBaskerville-Regular';
 }`
 const MontserratBold = `@font-face { 
-  src: url('./public/fonts/Montserrat-Bold.ttf')
-  font-family: 'Montserrat-Bold'
+  src: url('/public/fonts/Montserrat-Bold.ttf');
+  font-family: 'Montserrat-Bold';
 }`
 const MontserratBoldItalic = `@font-face { 
-  src: url('./public/fonts/Montserrat-BoldItalic.ttf')
-  font-family: 'Montserrat-BoldItalic'
+  src: url('/public/fonts/Montserrat-BoldItalic.ttf');
+  font-family: 'Montserrat-BoldItalic';
 }`
 const MontserratLight = `@font-face { 
-  src: url('./public/fonts/Montserrat-Light.ttf')
-  font-family: 'Montserrat-Light'
+  src: url('/public/fonts/Montserrat-Light.ttf');
+  font-family: 'Montserrat-Light';
 }`
 const MontserratLightItalic = `@font-face { 
-  src: url('./public/fonts/Montserrat-LightItalic.ttf')
-  font-family: 'Montserrat-LightItalic'
+  src: url('/public/fonts/Montserrat-LightItalic.ttf');
+  font-family: 'Montserrat-LightItalic';
 }`
 const NunitoSansBold = `@font-face { 
   src: url('/public/fonts/NunitoSans-Bold.ttf');
@@ -206,68 +206,68 @@ const NunitoSansRegular = `@font-face {
   font-family: 'NunitoSans-Regular';
 }`
 const PTSerifBold = `@font-face { 
-  src: url('./public/fonts/PTSerif-Bold.ttf')
-  font-family: 'PTSerif-Bold'
+  src: url('/public/fonts/PTSerif-Bold.ttf');
+  font-family: 'PTSerif-Bold';
 }`
 const PTSerifBoldItalic = `@font-face { 
-  src: url('./public/fonts/PTSerif-BoldItalic.ttf')
-  font-family: 'PTSerif-BoldItalic'
+  src: url('/public/fonts/PTSerif-BoldItalic.ttf');
+  font-family: 'PTSerif-BoldItalic';
 }`
 const PTSerifItalic = `@font-face { 
-  src: url('./public/fonts/PTSerif-Italic.ttf')
-  font-family: 'PTSerif-Italic'
+  src: url('/public/fonts/PTSerif-Italic.ttf');
+  font-family: 'PTSerif-Italic';
 }`
 const PTSerifRegular = `@font-face { 
-  src: url('./public/fonts/PTSerif-Regular.ttf')
-  font-family: 'PTSerif-Regular'
+  src: url('/public/fonts/PTSerif-Regular.ttf');
+  font-family: 'PTSerif';
 }`
 const PlayfairDisplayBlack = `@font-face { 
-  src: url('./public/fonts/PlayfairDisplay-Black.ttf')
-  font-family: 'PlayfairDisplay-Black'
+  src: url('/public/fonts/PlayfairDisplay-Black.ttf');
+  font-family: 'PlayfairDisplay-Black';
 }`
 const PlayfairDisplayBlackItalic = `@font-face { 
-  src: url('./public/fonts/PlayfairDisplay-BlackItalic.ttf')
-  font-family: 'PlayfairDisplay-BlackItalic'
+  src: url('/public/fonts/PlayfairDisplay-BlackItalic.ttf');
+  font-family: 'PlayfairDisplay-BlackItalic';
 }`
 const PlayfairDisplayBold = `@font-face { 
-  src: url('./public/fonts/PlayfairDisplay-Bold.ttf')
-  font-family: 'PlayfairDisplay-Bold'
+  src: url('/public/fonts/PlayfairDisplay-Bold.ttf');
+  font-family: 'PlayfairDisplay-Bold';
 }`
 const PlayfairDisplayBoldItalic = `@font-face { 
-  src: url('./public/fonts/PlayfairDisplay-BoldItalic.ttf')
-  font-family: 'PlayfairDisplay-BoldItalic'
+  src: url('/public/fonts/PlayfairDisplay-BoldItalic.ttf');
+  font-family: 'PlayfairDisplay-BoldItalic';
 }`
 const PlayfairDisplayItalic = `@font-face { 
-  src: url('./public/fonts/PlayfairDisplay-Italic.ttf')
-  font-family: 'PlayfairDisplay-Italic'
+  src: url('/public/fonts/PlayfairDisplay-Italic.ttf');
+  font-family: 'PlayfairDisplay-Italic';
 }`
 const PlayfairDisplayRegular = `@font-face { 
-  src: url('./public/fonts/PlayfairDisplay-Regular.ttf')
-  font-family: 'PlayfairDisplay-Regular'
+  src: url('/public/fonts/PlayfairDisplay-Regular.ttf');
+  font-family: 'PlayfairDisplay-Regular';
 }`
 const PlayfairDisplaySCBold = `@font-face { 
-  src: url('./public/fonts/PlayfairDisplaySC-Bold.ttf')
-  font-family: 'PlayfairDisplaySC-Bold'
+  src: url('/public/fonts/PlayfairDisplaySC-Bold.ttf');
+  font-family: 'PlayfairDisplaySC-Bold';
 }`
 const PlayfairDisplaySCBoldItalic = `@font-face { 
-  src: url('./public/fonts/PlayfairDisplaySC-BoldItalic.ttf')
-  font-family: 'PlayfairDisplaySC-BoldItalic'
+  src: url('/public/fonts/PlayfairDisplaySC-BoldItalic.ttf');
+  font-family: 'PlayfairDisplaySC-BoldItalic';
 }`
 const PoppinsExtraBold = `@font-face { 
-  src: url('./public/fonts/Poppins-ExtraBold.ttf')
-  font-family: 'Poppins-ExtraBold'
+  src: url('/public/fonts/Poppins-ExtraBold.ttf');
+  font-family: 'Poppins-ExtraBold';
 }`
 const PoppinsExtraBoldItalic = `@font-face { 
-  src: url('./public/fonts/Poppins-ExtraBoldItalic.ttf')
-  font-family: 'Poppins-ExtraBoldItalic'
+  src: url('/public/fonts/Poppins-ExtraBoldItalic.ttf');
+  font-family: 'Poppins-ExtraBoldItalic';
 }`
 const PoppinsItalic = `@font-face { 
-  src: url('./public/fonts/Poppins-Italic.ttf')
-  font-family: 'Poppins-Italic'
+  src: url('/public/fonts/Poppins-Italic.ttf');
+  font-family: 'Poppins-Italic';
 }`
 const PoppinsRegular = `@font-face { 
-  src: url('./public/fonts/Poppins-Regular.ttf')
-  font-family: 'Poppins-Regular'
+  src: url('/public/fonts/Poppins-Regular.ttf');
+  font-family: 'Poppins-Regular';
 }`
 
 if (Platform.OS === 'web') {
