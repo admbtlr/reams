6713fee7c1a297b068d70c1269d18283c18d4537
Adam Butler <ab@adam-butler.com>
2024-11-22 21:07:05 +0100

    fix: android stuff

--- android/app/src/main/java/app/reams/MainActivity.kt
+++ android/app/src/main/java/app/reams/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.alreadyapp.already
+package app.reams
  
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/app/reams/MainApplication.kt
+++ android/app/src/main/java/app/reams/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.alreadyapp.already
+package app.reams
  
 import android.app.Application
 import android.content.res.Configuration
