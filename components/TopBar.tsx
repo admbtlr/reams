c7e98557ffaf62eadbe2aced0bfdc319b4001a75
Adam Butler <ab@adam-butler.com>
2025-03-25 18:48:29 +0100

    fix: deepEqual on selector

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -9,9 +9,9 @@ import {
   View
 } from 'react-native'
 import { CommonActions } from '@react-navigation/native'
-import Svg, {Circle, G, Rect, Path } from 'react-native-svg'
-import {LinearGradient} from 'expo-linear-gradient'
-import { id } from '../utils'
+import Svg, { Circle, G, Rect, Path } from 'react-native-svg'
+import { LinearGradient } from 'expo-linear-gradient'
+import { deepEqual, id } from '../utils'
 import { getMargin, getStatusBarHeight } from '../utils/dimensions'
 import { fontSizeMultiplier } from '../utils/dimensions'
 import { isPortrait } from '../utils/dimensions'
@@ -48,7 +48,7 @@ const entities = require('entities')
 
 (* = container)
 */
- interface TopBarProps {
+interface TopBarProps {
   clampedAnimatedValue: Animated.AnimatedInterpolation<string | number> | undefined,
   emitter: {
     emit: (type: string) => null
@@ -58,10 +58,10 @@ const entities = require('entities')
   item: Item,
   key: string,
   navigation: any,
-  opacityAnim:  number | Animated.AnimatedInterpolation<string | number>,
+  opacityAnim: number | Animated.AnimatedInterpolation<string | number>,
   openFeedModal: (feedId: string) => void,
-  scrollAnim:  number | Animated.AnimatedInterpolation<string | number>,
-  titleTransformAnim:  number | Animated.AnimatedInterpolation<string | number>,
+  scrollAnim: number | Animated.AnimatedInterpolation<string | number>,
+  titleTransformAnim: number | Animated.AnimatedInterpolation<string | number>,
 }
 
 export default function TopBar({
@@ -75,9 +75,9 @@ export default function TopBar({
   titleTransformAnim,
   isVisible,
   index
-} : TopBarProps) {
+}: TopBarProps) {
   const displayMode = useSelector((state: RootState) => state.itemsMeta.display)
-  const items = useSelector((state: RootState) => getItems(state))
+  const items = useSelector((state: RootState) => getItems(state), deepEqual)
   const scrollRatios = useSelector((state: RootState) => getScrollRatio(state, item))
   const orientation = useSelector(state => state.config.orientation)
   const [numItems, setNumItems] = useState(items.length)
@@ -119,12 +119,12 @@ export default function TopBar({
     } else {
       return 'black'
     }
-  }  
+  }
 
   const getForegroundColor = () => {
     return 'white'
   }
-    
+
   const getBackgroundOpacityAnim = () => {
     if (isBackgroundTransparent && typeof scrollAnim == 'object') {
       return scrollAnim.interpolate({
@@ -137,178 +137,178 @@ export default function TopBar({
       return 1
     }
   }
-  
-  const feedTitle = feed?.title ? 
-    entities.decode(feed.title) : 
+
+  const feedTitle = feed?.title ?
+    entities.decode(feed.title) :
     (!!item && item.url?.split('/').length > 3) ? item.url.split('/')[2] : ''
 
-  const titleOpacity = typeof scrollAnim !== 'number' && isVisible ? 
+  const titleOpacity = typeof scrollAnim !== 'number' && isVisible ?
     scrollAnim.interpolate({
       inputRange: [0, 100, 140],
       outputRange: [0, 0, 1]
     }) : 0
-  
+
   const getCurrentContent = (): string => item.showMercuryContent ? 'mercury' : 'html'
-  const scrollRatio = item.showMercuryContent ? 
+  const scrollRatio = item.showMercuryContent ?
     scrollRatios?.mercury :
     scrollRatios?.html
 
   const hasScrollRatio = scrollRatio !== undefined && scrollRatio > 0
 
   return isOnboarding ? null :
-  (
-    <View>
-      <Animated.View
-        key={item ? item._id : id()}
-        pointerEvents={isVisible ? 'auto' : 'none'}
-        style={{
-          ...getStyles().textHolder,
-          flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'center',
-          opacity: opacityAnim,
-          overflow: 'hidden',
-          // paddingTop: 80,
-          position: 'absolute',
-          // top: hasNotchOrIsland() ? -240 : -260,
-          shadowOffset: {
-            width: 0,
-            height: 1
-          },
-          shadowRadius: 1,
-          shadowOpacity: 0.1,
-          shadowColor: 'rgba(0, 0, 0, 1)',
-          transform: [{
-            translateY: (isVisible && clampedAnimatedValue !== undefined) ? clampedAnimatedValue : 0
-          }]
-        }}
-      >
-        <Animated.View style={{
-          position: 'absolute',
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0,
-          backgroundColor: getBackgroundColor(),
-          opacity: getBackgroundOpacityAnim()
-        }}>
-        </Animated.View>
-        <BackButton
-          isDarkMode={isDarkMode}
-          navigation={navigation}
-          isSaved={displayMode === ItemType.saved}
-          color={getForegroundColor()}
-        />
-        <View style={{
-          // backgroundColor: 'yellow',
-          top: hasNotchOrIsland() && isPortrait() ? 44 : 22,
-          flex: 1,
-          marginLeft: 60 * fontSizeMultiplier(),
-          marginRight: 60 * fontSizeMultiplier()
-        }}>
-          <TouchableOpacity
-            key={`inner-{id()}`}
-            onPress={() => {
-              if (isOnboarding || displayMode === ItemType.saved) return
-              if (feed !== undefined) openFeedModal(feed._id)
-            }}
-            style={{
-              backgroundColor: 'transparent',
-              height: 70 * fontSizeMultiplier(),
-            }}>
-            <Animated.View style={{
-              flex: 1,
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: 64 * fontSizeMultiplier(),
-              marginTop: 0,
-              marginLeft: 0,
-              marginRight: 0,
-              opacity: isVisible && typeof scrollAnim !== 'number' ? 
-              scrollAnim.interpolate({
-                inputRange: [0, 120, 160],
-                outputRange: [1, 1, 0]
-              }) : 1,
-              transform: [{
-                translateY: titleTransformAnim || 0
-              }]
-            }}>
-                <View>
-                  <Favicon
-                    // this is a hot mess because newsletters have a url instead of a rootUrl
-                    url={feed?.rootUrl || feed?.url || item.url}
-                  />
-                </View>
-              <View style={{
-                flexDirection: 'column',
-                marginTop: 0,
+    (
+      <View>
+        <Animated.View
+          key={item ? item._id : id()}
+          pointerEvents={isVisible ? 'auto' : 'none'}
+          style={{
+            ...getStyles().textHolder,
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            opacity: opacityAnim,
+            overflow: 'hidden',
+            // paddingTop: 80,
+            position: 'absolute',
+            // top: hasNotchOrIsland() ? -240 : -260,
+            shadowOffset: {
+              width: 0,
+              height: 1
+            },
+            shadowRadius: 1,
+            shadowOpacity: 0.1,
+            shadowColor: 'rgba(0, 0, 0, 1)',
+            transform: [{
+              translateY: (isVisible && clampedAnimatedValue !== undefined) ? clampedAnimatedValue : 0
+            }]
+          }}
+        >
+          <Animated.View style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            backgroundColor: getBackgroundColor(),
+            opacity: getBackgroundOpacityAnim()
+          }}>
+          </Animated.View>
+          <BackButton
+            isDarkMode={isDarkMode}
+            navigation={navigation}
+            isSaved={displayMode === ItemType.saved}
+            color={getForegroundColor()}
+          />
+          <View style={{
+            // backgroundColor: 'yellow',
+            top: hasNotchOrIsland() && isPortrait() ? 44 : 22,
+            flex: 1,
+            marginLeft: 60 * fontSizeMultiplier(),
+            marginRight: 60 * fontSizeMultiplier()
+          }}>
+            <TouchableOpacity
+              key={`inner-{id()}`}
+              onPress={() => {
+                if (isOnboarding || displayMode === ItemType.saved) return
+                if (feed !== undefined) openFeedModal(feed._id)
+              }}
+              style={{
+                backgroundColor: 'transparent',
+                height: 70 * fontSizeMultiplier(),
               }}>
-                <Text
-                  style={{
-                    ...getStyles().feedName,
-                    fontSize: 12 * fontSizeMultiplier(),
-                    fontFamily: filter ?
-                      'IBMPlexSansCond' :
-                      'IBMPlexSansCond',
-                    color: getForegroundColor(),
-                    textAlign: 'left'
-                  }}
-                >{filter?.type ?
-                    (filter.type == 'category' ? 
-                      filter.title :
-                      'Feed') :
-                    (displayMode === ItemType.saved ?
-                      'Library' : 
-                      'Unread Stories')} • { index + 1 } / { numItems }</Text>
-                <Text
-                  numberOfLines={1}
-                  ellipsizeMode='tail'
-                  style={{
-                    ...getStyles().feedName,
-                    fontSize: 18 * fontSizeMultiplier(),
-                    lineHeight: 22 * fontSizeMultiplier(),
-                    fontFamily: filter ?
-                      'IBMPlexSansCond-Bold' :
-                      'IBMPlexSansCond',
-                    // color: this.getBorderBottomColor(item)
-                    color: getForegroundColor(),
-                    // height: 36,
-                    // paddingBottom: 15,
-                    textAlign: feed ?
-                      'left' : 'center',
-                    // textDecorationLine: displayMode === ItemType.saved ? 'none' : 'underline'
-                  }}
-                >
-                  {feedTitle}
-                </Text>
-              </View>
-            </Animated.View>
-            <Animated.View style={
-              Platform.OS === 'web' ? {
+              <Animated.View style={{
                 flex: 1,
-                flexDirection: 'column',
+                flexDirection: 'row',
                 justifyContent: 'center',
                 alignItems: 'center',
                 height: 64 * fontSizeMultiplier(),
                 marginTop: 0,
                 marginLeft: 0,
                 marginRight: 0,
-                opacity: titleOpacity,
-                position: 'absolute',
-                width: '100%',
-              } :
-              {
-                marginTop: 6,
-                opacity: typeof scrollAnim !== 'number' && isVisible ? 
+                opacity: isVisible && typeof scrollAnim !== 'number' ?
                   scrollAnim.interpolate({
-                    inputRange: [0, 100, 140],
-                    outputRange: [0, 0, 1]
-                  }) : 0,
-                position: 'absolute',
-                width: '100%',
-              }
-            }>
+                    inputRange: [0, 120, 160],
+                    outputRange: [1, 1, 0]
+                  }) : 1,
+                transform: [{
+                  translateY: titleTransformAnim || 0
+                }]
+              }}>
+                <View>
+                  <Favicon
+                    // this is a hot mess because newsletters have a url instead of a rootUrl
+                    url={feed?.rootUrl || feed?.url || item.url}
+                  />
+                </View>
+                <View style={{
+                  flexDirection: 'column',
+                  marginTop: 0,
+                }}>
+                  <Text
+                    style={{
+                      ...getStyles().feedName,
+                      fontSize: 12 * fontSizeMultiplier(),
+                      fontFamily: filter ?
+                        'IBMPlexSansCond' :
+                        'IBMPlexSansCond',
+                      color: getForegroundColor(),
+                      textAlign: 'left'
+                    }}
+                  >{filter?.type ?
+                    (filter.type == 'category' ?
+                      filter.title :
+                      'Feed') :
+                    (displayMode === ItemType.saved ?
+                      'Library' :
+                      'Unread Stories')} • {index + 1} / {numItems}</Text>
+                  <Text
+                    numberOfLines={1}
+                    ellipsizeMode='tail'
+                    style={{
+                      ...getStyles().feedName,
+                      fontSize: 18 * fontSizeMultiplier(),
+                      lineHeight: 22 * fontSizeMultiplier(),
+                      fontFamily: filter ?
+                        'IBMPlexSansCond-Bold' :
+                        'IBMPlexSansCond',
+                      // color: this.getBorderBottomColor(item)
+                      color: getForegroundColor(),
+                      // height: 36,
+                      // paddingBottom: 15,
+                      textAlign: feed ?
+                        'left' : 'center',
+                      // textDecorationLine: displayMode === ItemType.saved ? 'none' : 'underline'
+                    }}
+                  >
+                    {feedTitle}
+                  </Text>
+                </View>
+              </Animated.View>
+              <Animated.View style={
+                Platform.OS === 'web' ? {
+                  flex: 1,
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: 64 * fontSizeMultiplier(),
+                  marginTop: 0,
+                  marginLeft: 0,
+                  marginRight: 0,
+                  opacity: titleOpacity,
+                  position: 'absolute',
+                  width: '100%',
+                } :
+                  {
+                    marginTop: 6,
+                    opacity: typeof scrollAnim !== 'number' && isVisible ?
+                      scrollAnim.interpolate({
+                        inputRange: [0, 100, 140],
+                        outputRange: [0, 0, 1]
+                      }) : 0,
+                    position: 'absolute',
+                    width: '100%',
+                  }
+              }>
                 <Text
                   style={{
                     ...getStyles().feedName,
@@ -322,13 +322,13 @@ export default function TopBar({
                     textAlign: 'center',
                     textDecorationLine: 'underline'
                   }}
-                  >{ feedTitle }</Text>
+                >{feedTitle}</Text>
                 <Text
                   numberOfLines={2}
                   ellipsizeMode='tail'
                   style={{
                     ...getStyles().feedName,
-                    fontSize: 16  * fontSizeMultiplier(),
+                    fontSize: 16 * fontSizeMultiplier(),
                     lineHeight: 20 * fontSizeMultiplier(),
                     fontFamily: filter ?
                       'IBMPlexSansCond-Bold' :
@@ -343,33 +343,33 @@ export default function TopBar({
                 </Text>
               </Animated.View>
             </TouchableOpacity>
-        </View>
-        <View style={{
-          position: 'absolute',
-          right: 0,
-          bottom: 0,
-          width: 50
-        }}>
-          { hasScrollRatio &&
-            <TouchableOpacity 
-              onPress={ () => emitter.emit('scrollToRatio') }
-              style={{
-                flex: 1,
-                alignItems: 'flex-end',
-                justifyContent: 'flex-end',
-                marginBottom: 16 * fontSizeMultiplier(),
-                paddingRight: getMargin() / 2,
-              }}
-            >
-              {getRizzleButtonIcon('bookmark', getForegroundColor(), 'transparent', true, false)}
-            </TouchableOpacity>
-          }
-        </View>
-    </Animated.View>
-  </View>)
+          </View>
+          <View style={{
+            position: 'absolute',
+            right: 0,
+            bottom: 0,
+            width: 50
+          }}>
+            {hasScrollRatio &&
+              <TouchableOpacity
+                onPress={() => emitter.emit('scrollToRatio')}
+                style={{
+                  flex: 1,
+                  alignItems: 'flex-end',
+                  justifyContent: 'flex-end',
+                  marginBottom: 16 * fontSizeMultiplier(),
+                  paddingRight: getMargin() / 2,
+                }}
+              >
+                {getRizzleButtonIcon('bookmark', getForegroundColor(), 'transparent', true, false)}
+              </TouchableOpacity>
+            }
+          </View>
+        </Animated.View>
+      </View>)
 }
 
-const getStyles= () => {
+const getStyles = () => {
   return {
     topBar: {
       // flex: 1,
@@ -419,12 +419,12 @@ const getStyles= () => {
 
 interface BackButtonProps {
   color: string,
-  isDarkMode: boolean, 
-  isSaved: boolean, 
-  navigation: any 
+  isDarkMode: boolean,
+  isSaved: boolean,
+  navigation: any
 }
 
-const BackButton = ({ color, isDarkMode, isSaved , navigation: { navigate } }: BackButtonProps) => {
+const BackButton = ({ color, isDarkMode, isSaved, navigation: { navigate } }: BackButtonProps) => {
   const dispatch = useDispatch()
   const clearReadItems = () => dispatch({
     type: CLEAR_READ_ITEMS
@@ -448,14 +448,14 @@ const BackButton = ({ color, isDarkMode, isSaved , navigation: { navigate } }: B
           paddingBottom: 10,
           paddingLeft: 0,
           paddingRight: 50,
-          paddingTop: 10        
+          paddingTop: 10
         }}
       >
-        { getRizzleButtonIcon('back', color, 'transparent', true, false) }
+        {getRizzleButtonIcon('back', color, 'transparent', true, false)}
       </TouchableOpacity>
     </Animated.View>
   )
 }
 
 export const getTopBarHeight = () => getStatusBarHeight()// +
-  // (hasNotchOrIsland() ? 44 : 22)
+// (hasNotchOrIsland() ? 44 : 22)
