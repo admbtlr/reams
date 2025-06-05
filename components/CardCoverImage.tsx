5de949c98faf8ff6073b88692da03eadfe962155
Adam Butler <ab@adam-butler.com>
2025-06-05 13:27:12 +0200

    fix: crash when muting feeds

--- components/CardCoverImage.tsx
+++ components/CardCoverImage.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import {
   Animated, Image, Platform
 } from 'react-native'
-import {fileExists, getCachedCoverImagePath} from '../utils'
+import { fileExists, getCachedCoverImagePath } from '../utils'
 import { useSelector } from 'react-redux'
-import { RootState } from 'store/reducers'
+import { RootState } from '../store/reducers'
 import { useDispatch } from 'react-redux'
 import { REMOVE_CACHED_COVER_IMAGE, SET_CACHED_COVER_IMAGE } from '../store/feeds/types'
 import log from '../utils/log'
@@ -17,31 +17,29 @@ interface Props {
   testID?: string
 }
 
-export default function CardCoverImage ({feedId, itemId, width, height, testID}: Props) {
+export default function CardCoverImage({ feedId, itemId, width, height, testID }: Props) {
   const dispatch = useDispatch()
-  const feed = feedId ? useSelector((state: RootState) => state.feeds.feeds.find(f => f._id === feedId)) : undefined
-  const itemSaved = itemId ? useSelector((state: RootState) => state.itemsSaved.items.find(i => i._id === itemId)) : undefined
-  const itemUnread = itemId ? useSelector((state: RootState) => state.itemsUnread.items.find(i => i._id === itemId)) : undefined
+  const feed = useSelector((state: RootState) => feedId ? state.feeds.feeds.find(f => f._id === feedId) : undefined)
+  const itemSaved = useSelector((state: RootState) => itemId ? state.itemsSaved.items.find(i => i._id === itemId) : undefined)
+  const itemUnread = useSelector((state: RootState) => itemId ? state.itemsUnread.items.find(i => i._id === itemId) : undefined)
   const item = itemSaved || itemUnread
-  const feeds = useSelector((state: RootState) => state.feeds.feeds)
   const feedsLocal = useSelector((state: RootState) => state.feedsLocal)
   const unreadItems = useSelector((state: RootState) => state.itemsUnread.items)
-  const savedItems = useSelector((state: RootState) => state.itemsSaved.items)
   const cachedCoverImageId = !!feedId &&
-    feedsLocal.feeds.find(fl => fl._id === feed?._id)?.cachedCoverImageId    
-  const coverImageItem = !!feedId ? 
+    feedsLocal.feeds.find(fl => fl._id === feed?._id)?.cachedCoverImageId
+  const coverImageItem = !!feedId ?
     unreadItems.filter(i => i.feed_id === feedId).find(i => i.coverImageUrl) :
-    item?.hasCoverImage ? 
-      item : 
+    item?.hasCoverImage ?
+      item :
       undefined
-  const [imageDimensions, setImageDimensions] = useState({width: 0, height: 0}) // only used on web
+  const [imageDimensions, setImageDimensions] = useState({ width: 0, height: 0 }) // only used on web
   if (Platform.OS === 'web' && coverImageItem?.coverImageUrl) {
     Image.getSize(coverImageItem.coverImageUrl, (width, height) => {
       if (width !== imageDimensions.width || height !== imageDimensions.height) {
-        setImageDimensions({width, height})
+        setImageDimensions({ width, height })
       }
     })
-  }   
+  }
 
   const setCachedCoverImage = (sourceId: string, cachedCoverImageId: string) => {
     return dispatch({
@@ -67,7 +65,7 @@ export default function CardCoverImage ({feedId, itemId, width, height, testID}:
         removeCachedCoverImage(feedId || itemId || '')
       } else if (coverImageItem && !cachedCoverImageId) {
         setCachedCoverImage(feedId || itemId || '', coverImageItem?._id)
-      }  
+      }
     } catch (e) {
       log('maybeRemoveOrUpdateCoverImage', e)
     }
@@ -79,7 +77,7 @@ export default function CardCoverImage ({feedId, itemId, width, height, testID}:
   }, [coverImageItem, cachedCoverImageId])
 
   // shouldComponentUpdate (nextProps, nextState) {
-  //   return nextProps.feed?.feedLocal?.cachedCoverImageId !== 
+  //   return nextProps.feed?.feedLocal?.cachedCoverImageId !==
   //     this.props.feed?.feedLocal?.cachedCoverImageId
   // }
 
@@ -90,7 +88,7 @@ export default function CardCoverImage ({feedId, itemId, width, height, testID}:
       const imageId = cachedCoverImageId || coverImageItem?._id || item?._id
       return imageId ?
         getCachedCoverImagePath(imageId) :
-        null  
+        null
     }
   }
 
@@ -109,10 +107,8 @@ export default function CardCoverImage ({feedId, itemId, width, height, testID}:
           height,
           position: 'absolute',
         }}
-      /> 
+      />
     </Animated.View> :
     null
 
 }
-
-
