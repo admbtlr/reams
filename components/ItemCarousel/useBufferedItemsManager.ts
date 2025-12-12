f63e6f19c3c6e66a7fb3e23ee08e4b4b1b16d66d
Adam Butler <ab@adam-butler.com>
2025-12-12 09:44:52 +0100

    fix: stop updating buffer when items change

--- components/ItemCarousel/useBufferedItemsManager.ts
+++ components/ItemCarousel/useBufferedItemsManager.ts
@@ -4,6 +4,7 @@ import { Item, ItemType } from '@/store/items/types'
 import { getItems } from '@/utils/get-item'
 import { Feed } from '@/store/feeds/types'
 import { RootState } from '@/store/reducers'
+import { store } from '@/store'
 import { useUpdateFromRedux, useClearBuffer } from './bufferedItemsStore'
 
 // Singleton pattern - ensure initialization happens only once globally
@@ -17,26 +18,22 @@ export const useBufferedItemsManager = () => {
   const updateFromRedux = useUpdateFromRedux()
   const clearBuffer = useClearBuffer()
 
-  // Redux selectors with equality checks to prevent unnecessary updates
-  const items = useSelector(getItems, (oldItems: Item[], newItems: Item[]) => (
-    JSON.stringify(oldItems.map(i => i._id)) === JSON.stringify(newItems.map(i => i._id))
-  ))
-
-  const feeds = useSelector((state: RootState) => state.feeds.feeds, (oldFeeds: Feed[], newFeeds: Feed[]) => (
-    JSON.stringify(oldFeeds.map(f => f._id)) === JSON.stringify(newFeeds.map(f => f._id))
-  ))
-
   const displayMode = useSelector((state: RootState) => state.itemsMeta.display)
   const dispatch = useDispatch()
 
-  // Single effect - initialization and updates
+  // Single effect - initialization only (or when display mode changes)
   useEffect(() => {
     if (!isManagerInitialized) {
       isManagerInitialized = true
+      
+      // Get items/feeds only for initialization
+      const state = store.getState() as RootState
+      const items = getItems(state)
+      const feeds = state.feeds.feeds
+      
+      updateFromRedux(items, feeds, displayMode, dispatch)
     }
-
-    updateFromRedux(items, feeds, displayMode, dispatch)
-  }, [items, feeds, displayMode, dispatch, updateFromRedux])
+  }, [displayMode, dispatch, updateFromRedux])
 
   // Cleanup
   useEffect(() => {
@@ -47,8 +44,6 @@ export const useBufferedItemsManager = () => {
   }, [clearBuffer])
 
   return {
-    items,
-    feeds,
     displayMode
   }
 }
